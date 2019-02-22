94265e3971eb88552bbf9bc9b1dcd0e6fd62adf9
Sabrina Herrero <sabrinaherrero123@gmail.com>
2019-02-22 12:46:19 -0600

    Initial commit
    working on post to read json data

---
+++ botAPI.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify, request
+from flask_restful import reqparse, abort, Api, Resource
+
+app = Flask(__name__)
+api = Api(app)
+
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
+    def post(self):
+        print (request.json['text'])
+        return {'hello': 'world'}
+
+
+api.add_resource(HelloWorld, '/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
